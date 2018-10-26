a5f1e742de95a93c63665c297cedcde79164de25
Andrea Colarieti Tosti <colarietitosti@3points.de>
2018-10-26 17:19:24 +0200

    new EIP Projekt für WS 18/19

---
+++ EIP/WSProj/src/Addition.java
@@ -0,0 +1,12 @@
+public class Addition
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Dieses Programm addiert drei Zahlen." );
+        int x = 3;
+        int y = 5;
+        double z = 1.5;
+        System.out.print("Die Summe von " + x + ", " + y + " und " + z + " ist: ");
+        System.out.println(x+y+z);
+    }
+}
